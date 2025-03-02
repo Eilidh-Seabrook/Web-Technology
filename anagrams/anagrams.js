a318c6ae88263c94cf53e106c499fc37a603cc94
Eilidh-Seabrook <eilidhseabrook@gmail.com>
2025-03-02 22:04:03 +0000

    sean

--- anagrams/anagrams.js
+++ anagrams/anagrams.js
@@ -1,27 +1,82 @@
-const words = [
-  "apple",
-  "banana",
-  "tomato",
-  "orange",
-  "clementine"
-]
+const main = () => {
 
-// Implementation of the Fisher–Yates shuffle algorithm
-const shuffleArray = array => {
-  for (let n = array.length - 1; n > 0; n--) {
-    const i = Math.floor(Math.random() * (n + 1));
-    [array[n], array[i]] = [array[i], array[n]];
+  const words = [
+    "banana",
+    "apple",
+    "tomato",
+    "orange",
+    "clementine"
+  ]
+
+  // Implementation of the Fisher–Yates shuffle algorithm
+  const shuffleArray = array => {
+    for (let n = array.length - 1; n > 0; n--) {
+      const i = Math.floor(Math.random() * (n + 1));
+      [array[n], array[i]] = [array[i], array[n]];
+    }
   }
-}
 
-// Shuffles a given word using the above algorithm
-const scramble = word => {
-  let chars = word.split("");
-  shuffleArray(chars);
-  return chars.join("");
-}
+  const wordDisplay = document.getElementById("word");
+  const input = document.getElementById("input");
 
-// Test
-words.forEach(word => {
-  console.log(scramble(word));
-})
+  if (wordDisplay == null) {
+    console.error("Could not find element with id 'word'");
+    return;
+  }
+
+  if (input == null) {
+    console.error("Could not find element with id 'input'");
+    return;
+  }
+
+  const setWord = (index) => {
+    if (index > words.length - 1) {
+      return;
+    }
+
+    wordDisplay.innerHTML = "";
+
+    let word = words[index];
+
+    let scrambledWord = word.split("");
+    shuffleArray(scrambledWord);
+
+    for (let i = 0; i < scrambledWord.length; i++) {
+      const letter = scrambledWord[i];
+      let child = document.createElement("span");
+      let biasedRandom = Math.random() / 2 + i % 2 * 0.5;
+      child.style.setProperty("--random", biasedRandom)
+      child.innerText = letter;
+      wordDisplay.appendChild(child);
+    }
+
+    input.value = "";
+
+    if (input.minLength == word.length) {
+      return;
+    }
+
+    if (input.minLength < word.length) {
+      input.maxLength = word.length;
+      input.minLength = word.length;
+    } else {
+      input.minLength = word.length;
+      input.maxLength = word.length;
+    }
+    input.style.setProperty("--count", word.length);
+    input.parentElement?.style.setProperty("--underline", "'".padEnd(word.length + 1, "_").concat("'"));
+  }
+
+  let i = 0;
+
+  setWord(i);
+
+  input.addEventListener("change", event => {
+    const userInput = input.value.toLowerCase();
+    console.log(userInput);
+    if (userInput == words[i]) {
+      setWord(++i);
+    }
+  })
+}
+main();
